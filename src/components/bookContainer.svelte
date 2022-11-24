6780f4d9e268d4cf63e51317bcd2b1295f64f747
GibreelAbdullah <gibreel.khan@gmail.com>
2022-11-24 08:53:07 +0530

    Error and loading in card, removed old logo file

--- src/components/bookContainer.svelte
+++ src/components/bookContainer.svelte
@@ -4,12 +4,14 @@
 
 <div class="grid grid-cols-1 md:grid-cols-2 gap-8 p-4">
 	{#await dataPromise}
-		<div class="bookCard hvr-reveal">LOADING...</div>
+		<div class="card card-body hvr-reveal">LOADING...</div>
 	{:then data}
 		{#each data["collections"] as book}
 			<a class="card card-body text-center" href="/{book["name"]}">{book["eng-name"]} <br> {book["ara-name"]}</a>
 		{/each}
 	{:catch data}
-		<div class="bookCard hvr-reveal">Error...Could Not Load Data</div>
+		<div class="card card-body hvr-reveal">Error...Could Not Load Data</div>
 	{/await}
 </div>
+
+	
