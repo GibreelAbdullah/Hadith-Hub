0911b40827d9c68ff614299564641631d5e9c936
GibreelAbdullah <gibreel.khan@gmail.com>
2022-12-24 18:31:59 +0530

    Added google play link closes #25

--- src/lib/common/footer.svelte
+++ src/lib/common/footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="sticky top-[100vh] card card-body p-4 m-4  dark:bg-gradient-to-r dark:from-gray-800 dark:via-gray-800 dark:to-gray-900"
+  class="sticky top-[100vh] p-4 m-4"
 >
   <div class="flex space-x-4">
     <a
@@ -23,6 +23,16 @@
     >
       <SvgIcon name="github" fill="fill-white" />
       <span class="hidden md:inline-block ml-2 text-white pt-1">Github</span>
+    </a>
+    <a
+      class="btn bg-[#008c40] text-white"
+      href="https://play.google.com/store/apps/details?id=com.islamicproapps.hadithpro"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <SvgIcon name="playstore" fill="fill-white" />
+      <span class="hidden md:inline-block ml-2 text-white pt-1">Play Store</span
+      >
     </a>
   </div>
 </div>
