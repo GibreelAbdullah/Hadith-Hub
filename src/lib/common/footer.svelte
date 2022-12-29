88020f63c2f17d13c372f0677505b82c58452fc7
GibreelAbdullah <gibreel.khan@gmail.com>
2022-12-29 09:33:48 +0530

    About page and footer design changes

--- src/lib/common/footer.svelte
+++ src/lib/common/footer.svelte
@@ -1,45 +1,76 @@
 <script lang="ts">
   import SvgIcon from "$lib/components/svgIcon.svelte";
+  import { GradientHeading } from "@skeletonlabs/skeleton";
 </script>
-
-<div class="sticky top-[100vh] p-4 m-4">
-  <div class="flex space-x-4">
-    <a
-      class="btn bg-[#ea4335] text-white"
-      href="mailto:gibreel.khan@gmail.com"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <SvgIcon name="mail" fill="fill-white" />
-      <span class="hidden md:inline-block ml-2 text-white pt-1">Mail</span>
-    </a>
-    <a
-      class="btn bg-[#6e5494] text-white"
-      href="https://github.com/GibreelAbdullah/Hadith-Hub"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <SvgIcon name="github" fill="fill-white" />
-      <span class="hidden md:inline-block ml-2 text-white pt-1">Github</span>
-    </a>
-    <a
-      class="btn bg-[#1da1f2] "
-      href="https://twitter.com/TheHadithHub"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <SvgIcon name="twitter" fill="fill-white" />
-      <span class="hidden md:inline-block ml-2 text-white pt-1">Twitter</span>
-    </a>
-
-    <a
-      class="btn bg-[#008c40] text-white"
-      href="https://play.google.com/store/apps/details?id=com.islamicproapps.hadithpro"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <SvgIcon name="playstore" fill="fill-white" />
-      <span class="hidden md:inline-block ml-2 text-white pt-1">PlayStore</span>
-    </a>
-  </div>
+<div class="p-8 sticky top-[100vh]">
+  <section
+    class="flex flex-col lg:flex-row justify-between items-center lg:items-start space-y-5 lg:space-y-0"
+  >
+    <GradientHeading
+      class="md:text-3xl"
+      tag="h1"
+      direction="bg-gradient-to-r"
+      from="from-primary-500"
+      to="to-accent-500"
+      >HadithHub
+    </GradientHeading>
+    <div class="flex space-x-4">
+      <a
+        class="btn bg-[#ea4335] text-white"
+        href="mailto:gibreel.khan@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <SvgIcon name="mail" fill="fill-white" />
+        <span class="hidden lg:inline-block ml-2 text-white pt-1">Mail</span>
+      </a>
+      <a
+        class="btn bg-[#6e5494] text-white"
+        href="https://github.com/GibreelAbdullah/Hadith-Hub"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <SvgIcon name="github" fill="fill-white" />
+        <span class="hidden lg:inline-block ml-2 text-white pt-1">Github</span>
+      </a>
+      <a
+        class="btn bg-[#1da1f2] "
+        href="https://twitter.com/TheHadithHub"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <SvgIcon name="twitter" fill="fill-white" />
+        <span class="hidden lg:inline-block ml-2 text-white pt-1">Twitter</span>
+      </a>
+      <a
+        class="btn bg-[#008c40] text-white"
+        href="https://play.google.com/store/apps/details?id=com.islamicproapps.hadithpro"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <SvgIcon name="playstore" fill="fill-white" />
+        <span class="hidden lg:inline-block ml-2 text-white pt-1"
+          >PlayStore</span
+        >
+      </a>
+    </div>
+  </section>
+  <section class="pt-8 flex flex-row justify-center lg:justify-start items-center space-y-0">
+    <div>
+      <span class="float-left pr-8">Contributors</span>
+      <p class="text-xs pr-8">
+        <a
+          href="https://github.com/GibreelAbdullah/hadith-api/graphs/contributors"
+          target="_blank"
+          rel="noreferrer">Data</a
+        > <span class="opacity-10 mx-4">|</span>
+        <a
+          href="https://github.com/GibreelAbdullah/HadithHub/graphs/contributors"
+          target="_blank"
+          rel="noreferrer">UI</a
+        >
+      </p>
+    </div>
+    <a class="float-right pl-8" href="/about">About</a>
+  </section>
 </div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+	import SvgIcon from "$lib/components/svgIcon.svelte";
+	import { Divider } from "@skeletonlabs/skeleton";
+</script>
+
+<div class="card card-body p-4 m-4 flex-wrap">
+	<div class="text-center fill-black dark:fill-white">
+		<SvgIcon class="md:w-60 w-40" name="bismillah" />
+	</div>
+	<div class="text-center fill-black dark:fill-white p-4">
+		With the name of Allah, the All-Merciful, the Very-Merciful.
+	</div>
+	<div class="px-6 py-2">
+		All praise and thanks are due to Allah, the Master and King of
+		existence, the All-Merciful, Beneficent, who has power over all things.
+		May peace and blessings be upon His final Messenger, Sayyidina Muhammad ﷺ, and
+		his Family, and his Companions.
+	</div>
+	<div class="text-xl p-4">About<Divider /></div>
+	<div class="px-6 py-2">
+		HadithHub.com started in October 2022. It aims to make it easy for
+		anyone to read, study, and learn the ahadith of Sayyidina Rasulullah ﷺ.
+		This project is open source.
+	</div>
+	<div class="px-6 py-2">
+		Frontend source code can be found
+		<a href="https://github.com/GibreelAbdullah/Hadith-Hub"> here </a> and
+		all the data in the app can be found
+		<a href="https://github.com/GibreelAbdullah/hadith-api"> here</a>. We
+		will be grateful if you contribute in this project to make it better.
+	</div>
+	<div class="px-6 py-2">
+		It would be a grave injustice to omit the mention of
+		<a href="https://github.com/fawazahmed0"> Fawaz Ahmed </a> who made the enormous
+		effort in collecting Ahadith in multiple languages and their gradings by
+		multiple scholars. This project wouldn't be possible without his efforts.
+		May Allah ﷻ reward him immensely.
+	</div>
+
+	<div class="text-xl p-4">Credits <Divider /></div>
+	<div class="px-6 py-2">
+		<a href="https://github.com/fawazahmed0" class="font-bold">
+			Fawaz Ahmed
+		</a>: For collecting all the
+		<a href="https://github.com/fawazahmed0/hadith-api">Ahadith data</a>. A
+		complete list of sources of the data can be found
+		<a
+			href="https://github.com/fawazahmed0/hadith-api/blob/1/References.md"
+		>
+			here
+		</a>.
+	</div>
+	<div class="px-6 py-2">
+		<a
+			href="https://github.com/GibreelAbdullah/HadithHub/graphs/contributors"
+			class="font-bold"
+		>
+			Contributors - UI</a
+		>: Contributors who helped in creating the website.
+	</div>
+	<div class="px-6 py-2">
+		<a
+			href="https://github.com/GibreelAbdullah/hadith-api/graphs/contributors"
+			class="font-bold"
+		>
+			Contributors - Data</a
+		>: Contributors who helped in collecting hadith/gradings or correcting
+		them.
+	</div>
+	<div class="px-6 py-2">
+		<a href="https://svelte.dev/" class="font-bold"> Svelte </a>: A
+		front-end, open-source JavaScript framework for making interactive
+		webpages.
+	</div>
+	<div class="px-6 py-2">
+		<a href="https://www.skeleton.dev/" class="font-bold"> Skeleton </a>: A
+		fully featured UI Toolkit for svelte.
+	</div>
+	<div class="px-6 py-2">
+		<a href="https://github.com/bubkoo/html-to-image/" class="font-bold">
+			html-to-image
+		</a>: A javascript library to convert html to image, essentially
+		allowing us to download screenshots of Ahadith.
+	</div>
+</div>
