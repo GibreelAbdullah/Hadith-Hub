c1211b022fb952c1d43992f257d237326a0abe0d
Gibreel Abdullah <gibreel.khan@gmail.com>
2024-01-14 22:56:40 +0530

    About section from api

--- src/lib/common/footer.svelte
+++ src/lib/common/footer.svelte
@@ -1,71 +1,73 @@
 <script lang="ts">
-  import SvgIcon from "$lib/common/svgIcon.svelte";
+	import SvgIcon from '$lib/common/svgIcon.svelte';
 </script>
+
 <div class="p-8 sticky top-[100vh]">
-  <section
-    class="flex flex-col lg:flex-row justify-between items-center lg:items-start space-y-5 lg:space-y-0"
-  >
-  <span class="pr-4">
-    <SvgIcon class="!w-10" name="icon" />
-    <SvgIcon class="!w-40" name="hadithHub" />
-  </span>
-    <div class="px-4 logo-cloud grid-cols-4 gap-1">
-      <a
-        class="btn bg-[#ea4335] text-white"
-        href="mailto:gibreel.khan@gmail.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <SvgIcon name="mail" fill="fill-white" />
-        <span class="hidden lg:inline-block ml-2 text-white pt-1">Mail</span>
-      </a>
-      <a
-        class="btn bg-[#6e5494] text-white"
-        href="https://github.com/GibreelAbdullah/Hadith-Hub"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <SvgIcon name="github" fill="fill-white" />
-        <span class="hidden lg:inline-block ml-2 text-white pt-1">Github</span>
-      </a>
-      <a
-        class="btn bg-[#1da1f2] "
-        href="https://twitter.com/TheHadithHub"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <SvgIcon name="twitter" fill="fill-white" />
-        <span class="hidden lg:inline-block ml-2 text-white pt-1">Twitter</span>
-      </a>
-      <a
-        class="btn bg-[#008c40] text-white"
-        href="https://play.google.com/store/apps/details?id=com.islamicproapps.hadithpro"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <SvgIcon name="playstore" fill="fill-white" />
-        <span class="hidden lg:inline-block ml-2 text-white pt-1"
-          >PlayStore</span
-        >
-      </a>
-    </div>
-  </section>
-  <section class="pt-8 flex flex-row justify-center lg:justify-start items-center space-y-0">
-    <div>
-      <span class="float-left">Contributors</span>
-      <p class="text-xs">
-        <a
-          href="https://github.com/GibreelAbdullah/hadith-api/graphs/contributors"
-          target="_blank"
-          rel="noreferrer">Data</a
-        > <span class="opacity-10 mx-4">|</span>
-        <a
-          href="https://github.com/GibreelAbdullah/HadithHub/graphs/contributors"
-          target="_blank"
-          rel="noreferrer">UI</a
-        >
-      </p>
-    </div>
-    <a class="float-right pl-8" href="/about">About</a>
-  </section>
+	<section
+		class="flex flex-col lg:flex-row justify-between items-center lg:items-start space-y-5 lg:space-y-0"
+	>
+		<span class="pr-4">
+			<SvgIcon class="!w-10" name="icon" />
+			<SvgIcon class="!w-40" name="hadithHub" />
+		</span>
+		<div class="px-4 logo-cloud grid-cols-4 gap-1">
+			<a
+				class="btn bg-[#ea4335] text-white"
+				href="mailto:gibreel.khan@gmail.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<SvgIcon name="mail" fill="fill-white" />
+				<span class="hidden lg:inline-block ml-2 text-white pt-1">Mail</span>
+			</a>
+			<a
+				class="btn bg-[#6e5494] text-white"
+				href="https://github.com/GibreelAbdullah/Hadith-Hub"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<SvgIcon name="github" fill="fill-white" />
+				<span class="hidden lg:inline-block ml-2 text-white pt-1">Github</span>
+			</a>
+			<a
+				class="btn bg-[#1da1f2]"
+				href="https://twitter.com/TheHadithHub"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<SvgIcon name="twitter" fill="fill-white" />
+				<span class="hidden lg:inline-block ml-2 text-white pt-1">Twitter</span>
+			</a>
+			<a
+				class="btn bg-[#008c40] text-white"
+				href="https://play.google.com/store/apps/details?id=com.islamicproapps.hadithpro"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<SvgIcon name="playstore" fill="fill-white" />
+				<span class="hidden lg:inline-block ml-2 text-white pt-1">PlayStore</span>
+			</a>
+		</div>
+	</section>
+	<section class="pt-8 flex flex-row justify-center lg:justify-start items-center space-y-0">
+		<div>
+			<span class="float-left">Contributors</span>
+			<p class="text-xs">
+				<a
+          class="anchor"
+					href="https://github.com/GibreelAbdullah/hadith-api/graphs/contributors"
+					target="_blank"
+					rel="noreferrer">Data</a
+				>
+				<span class="opacity-10 mx-4">|</span>
+				<a
+          class="anchor"
+					href="https://github.com/GibreelAbdullah/HadithHub/graphs/contributors"
+					target="_blank"
+					rel="noreferrer">UI</a
+				>
+			</p>
+		</div>
+		<a class="anchor float-right pl-8" href="/about">About</a>
+	</section>
 </div>
