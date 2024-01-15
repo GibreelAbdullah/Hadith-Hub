7dea36963a002163621701d14a8801ffe25601f7
Gibreel Abdullah <gibreel.khan@gmail.com>
2024-01-15 09:45:14 +0530

    Fixed purged css issue

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { urlPrefix } from '$lib/common/constants';
 	import SvgIcon from '$lib/common/svgIcon.svelte';
-
 	let htmlPromise = fetch(urlPrefix + '/updates/about/about.html').then((response) => {
-		console.dir(response);
 		return response.text();
 	});
 </script>
@@ -13,7 +11,7 @@
 		<SvgIcon class="!w-40" name="bismillah" />
 	</div>
 	{#await htmlPromise}
-		<div class="card p-4 m-4">
+		<div class="card anchor dark:fill-white fill-black font-bold p-4 px-6 py-2 text-center text-xl">
 			<div class="hadithGroup font-medium p-2 grid">
 					<div class="placeholder animate-pulse" />
 			</div>
